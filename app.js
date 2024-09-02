48a528c3aab7a3baf900e6c5044f8f19b4fcb1da
usernmae <afshaanqasim07@gmail.com>
2024-09-02 12:38:29 -0700

    done

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // function
+const dice = document.querySelector("#dice")
+const btn = document.querySelector("#btn")
+const rollthedice = () => {
+const dice = document.getElementById("dice");
+const roll = Math.floor(Math.random() * 6) + 1;
+dice.textContent = roll;
+};
+btn.addEventListener("click", rollthedice);
+
